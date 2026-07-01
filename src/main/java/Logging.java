e132bfd0da0ecbdaf2b1871573a9d97d16af198e
Prakash V <prakashseervi1503@gmail.com>
2026-07-01 11:57:16 +0530

    added conf Level

--- src/main/java/Logging.java
+++ src/main/java/Logging.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.config.Configurator;
 
 public class Logging {
     private static final Logger logging = LogManager.getLogger(Logging.class);
@@ -19,6 +21,8 @@ public class Logging {
         logging.info("Clicking Download");
         driver.findElement(By.linkText("Downloads")).click();
         logging.info("URL:- "+ driver.getCurrentUrl());
+
+        Configurator.setLevel(Logging.class.getName(),Level.OFF);
 
         logging.warn("Warning");
         driver.close();

4866af65dbf673b8051d3352e81b9ecb7c228285
Prakash V <prakashseervi1503@gmail.com>
2026-07-01 11:49:06 +0530

    logging

--- src/main/java/Logging.java
+++ src/main/java/Logging.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Loggin {
+public class Logging {
+    private static final Logger logging = LogManager.getLogger(Logging.class);
+
+    public static void main(String[] args){
+        logging.trace("Opening Chrome Browser");
+        logging.info("Selenium Page Automation");
+
+        WebDriver driver =  new ChromeDriver();
+
+        driver.get("https://www.selenium.dev/");
+
+        logging.info("Clicking Download");
+        driver.findElement(By.linkText("Downnloads")).click();
+        logging.info("URL:- "+ driver.getCurrentUrl());
+
+        logging.warn("Warning");
+        driver.close();
+    }
 }

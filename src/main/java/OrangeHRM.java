b5eee2eb04174ae3b6339e170dbd081f42a714a2
Prakash V <prakashseervi1503@gmail.com>
2026-07-09 09:29:36 +0530

    added screencapture

--- src/main/java/OrangeHRM.java
+++ src/main/java/OrangeHRM.java
@@ -14,6 +14,19 @@ public class OrangeHRM {
         test.attachReporter(sp);
 
         ExtentTest tt = test.createTest("HR Management");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        Thread.sleep(3000);
+
+        capture(driver,tt,"01_Home");
+
+        driver.findElement(By.name("username")).sendKeys("Admin");
+        driver.findElement(By.name("password")).sendKeys("admin123");
+
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+        Thread.sleep(3000);
+        capture(driver,tt,"02_Dashboard");
     }
 
     public static void capture(WebDriver driver,ExtentTest test,String name) throws Exception{
@@ -22,6 +35,9 @@ public class OrangeHRM {
         File dest = new File("reports" + name + ".png");
 
         Files.copy(src.toPath(),dest.toPath(),StandardCopyOption.REPLACE_EXISTING);
+
+        test.addScreenCaptureFromPath(name + ".png");
+
     }
 
 }
